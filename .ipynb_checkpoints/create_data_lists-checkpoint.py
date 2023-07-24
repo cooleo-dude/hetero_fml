978c7537f95b25f93ab6e7b2d6589661ba5e3664
cooleo-dude <coleleo00@gmail.com>
2023-07-24 17:14:05 -0400

    Init

---
+++ .ipynb_checkpoints/create_data_lists-checkpoint.py
@@ -0,0 +1,13 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+#    create_data_lists(voc07_path='/media/ssd/ssd data/VOC2007',
+#                      voc12_path='/media/ssd/ssd data/VOC2012',
+#                      output_folder='./')
+    create_data_lists(voc07_path='/home/cole/Documents/dumbgy/a-PyTorch-Tutorial-to-Object-Detection/data/VOCdevkit/VOC2007',
+                      voc12_path='/home/cole/Documents/dumbgy/a-PyTorch-Tutorial-to-Object-Detection/data/VOCdevkit/VOC2012',
+                      output_folder='./')
+
+
+#/home/cole/Documents/dumbgy/a-PyTorch-Tutorial-to-Object-Detection/data/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
+#/home/cole/Documents/dumbgy/a-PyTorch-Tutorial-to-Object-Detection/data/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
